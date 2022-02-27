aa36af148bc5a94f8a9dcd921a494a8ba8c0e921
rufaidulk <rufaidulk999@gmail.com>
2022-02-27 10:08:03 +0530

    user service fix test cases

--- userservice/tests/auth_test.go
+++ userservice/tests/auth_test.go
@@ -10,12 +10,14 @@ import (
 	"testing"
 	"userservice/controllers"
 	"userservice/helper"
+	"userservice/middlewares"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 var jwt string
+var userDetails interface{}
 
 func TestMain(m *testing.M) {
 	fmt.Println("Unit tests starting...")
@@ -36,8 +38,8 @@ func TestRegistration(t *testing.T) {
 	if assert.NoError(t, controllers.Registration(ctx)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
 	}
-	fmt.Println("Response:")
-	fmt.Println(rec.Body.String())
+	// fmt.Println("Response:")
+	// fmt.Println(rec.Body.String())
 }
 
 func TestLogin(t *testing.T) {
@@ -52,31 +54,55 @@ func TestLogin(t *testing.T) {
 	if assert.NoError(t, controllers.Login(ctx)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
-	fmt.Println("Response:")
-	fmt.Println(rec.Body.String())
+	// fmt.Println("Response:")
+	// fmt.Println(rec.Body.String())
 	x := helper.SuccessResponse{}
 	json.Unmarshal(rec.Body.Bytes(), &x)
 	data := x.Data.(map[string]interface{})
 	jwt = data["token"].(string)
 }
 
-//todo:: to be debugged
-// func TestAuthorization(t *testing.T) {
-// 	// token := "Bearer "
-// 	// token += jwt
-// 	token := fmt.Sprintf("Bearer %s", jwt)
-// 	fmt.Println(token)
-// 	e := echo.New()
-// 	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(""))
-// 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-// 	req.Header.Set(echo.HeaderAuthorization, token)
-// 	rec := httptest.NewRecorder()
-// 	ctx := e.NewContext(req, rec)
-// 	ctx.Set("mode", "testing")
+func TestInvalidJwtOnAuthMiddleware(t *testing.T) {
+	invalidJwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDU2MzU0MDksImlzcyI6ImFkbWluIiwidXVpZCI6IjE0YTAxZTUzLTkyY2UtNDMwNi1hZmVmLTI1OGM0MTFhMzEzNyIsIm5hbWUiOiJhZGkiLCJlbWFpbCI6ImFkaUBtYWlsLmNvbSJ9.KzZDfVwvr6wLwkfcNZuL84Wpj4XxOazOvg2q9bdDa8g"
+	token := fmt.Sprintf("Bearer %s", invalidJwt)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	req.Header.Set(echo.HeaderAuthorization, token)
+	res := httptest.NewRecorder()
+	ctx := e.NewContext(req, res)
 
-// 	if assert.NoError(t, controllers.AuthorizeUser(ctx)) {
-// 		assert.Equal(t, http.StatusOK, rec.Code)
-// 	}
-// 	fmt.Println("Response:")
-// 	fmt.Println(rec.Body.String())
-// }
+	mw := middlewares.UserJwtMiddleware
+	if assert.NoError(t, mw(controllers.AuthorizeUser)(ctx)) {
+		assert.Equal(t, http.StatusUnauthorized, res.Code)
+	}
+}
+func TestValidJwtOnAuthMiddleware(t *testing.T) {
+	token := fmt.Sprintf("Bearer %s", jwt)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", nil)
+	req.Header.Set(echo.HeaderAuthorization, token)
+	res := httptest.NewRecorder()
+	ctx := e.NewContext(req, res)
+	mw := middlewares.UserJwtMiddleware
+
+	if assert.NoError(t, mw(controllers.AuthorizeUser)(ctx)) {
+		assert.Equal(t, http.StatusOK, res.Code)
+		userDetails = ctx.Get("userDetails")
+	}
+}
+
+func TestAuthorization(t *testing.T) {
+	token := fmt.Sprintf("Bearer %s", jwt)
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, token)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.Set("mode", "testing")
+	ctx.Set("userDetails", userDetails)
+
+	if assert.NoError(t, controllers.AuthorizeUser(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
